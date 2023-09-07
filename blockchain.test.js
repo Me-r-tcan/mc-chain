e44db27792be5ff8320faa86dee1d0ee2e2955db
Mertcan bas <mertcanbas35@gmail.com>
2023-09-07 16:26:44 +0300

    add chain validation and replace chain

--- blockchain.test.js
+++ blockchain.test.js
@@ -2,10 +2,11 @@ const Blockchain = require('./blockchain');
 const Block = require('./block');
 
 describe('Blockchain', () => {
-  let bc;
+  let bc, bc2;
 
   beforeEach(() => {
     bc = new Blockchain();
+    bc2 = new Blockchain();
   });
 
   it('starts with a genesis block', () => {
@@ -17,5 +18,38 @@ describe('Blockchain', () => {
     bc.addBlock(data);
 
     expect(bc.chain[bc.chain.length - 1].data).toEqual(data);
+  });
+
+  it('validates a valid chain', () => {
+    bc2.addBlock('foo');
+
+    expect(bc.isValidChain(bc2.chain)).toBe(true);
+  });
+
+  it('invalidates a chain with a corrupt genesis block', () => {
+    bc2.chain[0].data = 'corrupted data';
+
+    expect(bc.isValidChain(bc2.chain)).toBe(false);
+  });
+
+  it('invalidates a chain with a corrupt chain', () => {
+    bc2.addBlock('foo');
+    bc2.chain[1].data = 'corrupted data';
+
+    expect(bc.isValidChain(bc2.chain)).toBe(false);
+  });
+
+  it('replaces the chain with a valid chain', () => {
+    bc2.addBlock('foo');
+    bc.replaceChain(bc2.chain);
+
+    expect(bc.chain).toEqual(bc2.chain);
+  });
+
+  it('does not replace the chain with one of less than or equal to length', () => {
+    bc.addBlock('foo');
+    bc.replaceChain(bc2.chain);
+
+    expect(bc.chain).not.toEqual(bc2.chain);
   });
 });
